1c5d5b7cb683f3a53c0d63bf9af9b712867980ab
mohammedrahamadulla <rahamadulla786.md@gmail.com>
2023-02-05 10:00:51 +0530

    Adding VPC Peering

--- vpc-peering.tf
+++ vpc-peering.tf
@@ -32,7 +32,7 @@ resource "aws_route" "peering" {
 
 resource "aws_route" "peeringfromansible" {
   route_table_id            = data.aws_route_table.ansible_vpc_rt.id
-  destination_cidr_block    = "10.1.0.0/16"
+  destination_cidr_block    = "172.16.0.0/16"
   vpc_peering_connection_id = aws_vpc_peering_connection.foo.id
   depends_on                = [aws_route_table.terraform-public]
 }
