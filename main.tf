da01afadf76abd922afba0575977b019ed77f2a7
mohammedrahamadulla <rahamadulla786.md@gmail.com>
2023-04-10 21:17:51 +0530

    added instance & .gitignore

--- main.tf
+++ main.tf
@@ -160,7 +160,7 @@ resource "aws_instance" "web-4" {
     #ami = "${data.aws_ami.my_ami.id}"
     availability_zone = "us-east-1a"
     instance_type = "t2.micro"
-    key_name = "Laptop_key2"
+    key_name = var.key_name
     subnet_id = "${aws_subnet.subnet1-public.id}"
     vpc_security_group_ids = ["${aws_security_group.allow_all.id}"]
     associate_public_ip_address = true	
@@ -178,7 +178,7 @@ resource "aws_instance" "web-5" {
     #ami = "${data.aws_ami.my_ami.id}"
     availability_zone = "us-east-1a"
     instance_type = "t2.micro"
-    key_name = "Laptop_key2"
+    key_name = var.key_name
     subnet_id = "${aws_subnet.subnet1-public.id}"
     vpc_security_group_ids = ["${aws_security_group.allow_all.id}"]
     associate_public_ip_address = true	
@@ -196,7 +196,7 @@ resource "aws_instance" "web-6" {
     #ami = "${data.aws_ami.my_ami.id}"
     availability_zone = "us-east-1a"
     instance_type = "t2.micro"
-    key_name = "Laptop_key2"
+    key_name = var.key_name
     subnet_id = "${aws_subnet.subnet1-public.id}"
     vpc_security_group_ids = ["${aws_security_group.allow_all.id}"]
     associate_public_ip_address = true	
